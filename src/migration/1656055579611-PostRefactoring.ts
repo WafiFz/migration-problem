f83461fc01e75ebacb69b1946190fee8993b8697
Wafi Fahruzzaman <wafi20002@mail.unpad.ac.id>
2022-07-01 13:30:05 +0700

    yml

--- src/migration/1656055579611-PostRefactoring.ts
+++ src/migration/1656055579611-PostRefactoring.ts
@@ -12,7 +12,7 @@ export class PostRefactoring1656055579611 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: "question",
+                name: "books",
                 columns: [
                     {
                         name: "id",
@@ -20,62 +20,65 @@ export class PostRefactoring1656055579611 implements MigrationInterface {
                         isPrimary: true,
                     },
                     {
-                        name: "name",
-                        type: "varchar",
+                        name: "title",
+                        type: "character varying",
+                    },
+                    {
+                        name: "publisher",
+                        type: "character varying",
+                    },
+                    {
+                        name: "price",
+                        type: "numeric",
                     },
                 ],
             }),
             true,
         )
 
-        await queryRunner.createIndex(
-            "question",
-            new TableIndex({
-                name: "IDX_QUESTION_NAME",
-                columnNames: ["name"],
-            }),
-        )
+        // await queryRunner.createTable(
+        //     new Table({
+        //         name: "answer",
+        //         columns: [
+        //             {
+        //                 name: "id",
+        //                 type: "int",
+        //                 isPrimary: true,
+        //             },
+        //             {
+        //                 name: "title",
+        //                 type: "character varying",
+        //             },
+        //             {
+        //                 name: "publisher",
+        //                 type: "character varying",
+        //             },
+        //             {
+        //                 name: "price",
+        //                 type: "number",
+        //             },
+        //         ],
+        //     }),
+        //     true,
+        // )
 
-        await queryRunner.createTable(
-            new Table({
-                name: "answer",
-                columns: [
-                    {
-                        name: "id",
-                        type: "int",
-                        isPrimary: true,
-                    },
-                    {
-                        name: "name",
-                        type: "varchar",
-                    },
-                    {
-                        name: "created_at",
-                        type: "timestamp",
-                        default: "now()",
-                    },
-                ],
-            }),
-            true,
-        )
+        // await queryRunner.addColumn(
+        //     "answer",
+        //     new TableColumn({
+        //         name: "questionId",
+        //         type: "int",
+        //     }),
+        // )
 
-        await queryRunner.addColumn(
-            "answer",
-            new TableColumn({
-                name: "questionId",
-                type: "int",
-            }),
-        )
-
-        await queryRunner.createForeignKey(
-            "answer",
-            new TableForeignKey({
-                columnNames: ["questionId"],
-                referencedColumnNames: ["id"],
-                referencedTableName: "question",
-                onDelete: "CASCADE",
-            }),
-        )
+        // await queryRunner.createForeignKey(
+        //     "answer",
+        //     new TableForeignKey({
+        //         columnNames: ["questionId"],
+        //         referencedColumnNames: ["id"],
+        //         referencedTableName: "question",
+        //         onDelete: "CASCADE",
+        //     }),
+        // )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
